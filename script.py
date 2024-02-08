9a86bb2a39e690b441dbe0e84dff6e635e0de8dc
researchersec <109282836+researchersec@users.noreply.github.com>
2024-02-08 15:51:27 +0100

    Update script.py

--- script.py
+++ script.py
@@ -47,7 +47,7 @@ if response.status_code == 201:
 
         # Copy the content of timestamp.json to latest.json
         try:
-            shutil.copyfile(file_path, latest_file_path)
+            shutil.move(file_path, latest_file_path)
             print("latest.json has been updated with the same data as timestamp.json.")
         except FileNotFoundError:
             print(f"Error: File {file_path} not found.")
